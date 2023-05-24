41f0da6850a50f9803445ced59e5d2d2ec782629
Gamesacar <95244339+Gamesacar@users.noreply.github.com>
2023-05-23 21:12:24 -0500

    Update 1_Space Scape.py

--- pages/1_Space Scape.py
+++ pages/1_Space Scape.py
@@ -38,7 +38,7 @@ url = 'https://unity.com/es/download'
 if st.button('USA UNITY'):
     webbrowser.open_new_tab(url)
 
-video_file = open('C:/Users/sajoh/Desktop/multipagidaspace/video.mp4','rb')
+video_file = open('video.mp4','rb')
 video_bytes = video_file.read()
 st.video(video_file)
 
